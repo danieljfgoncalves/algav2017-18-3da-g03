839206aebd7b7ff9f81ae848c32a9019ee386f3a
Daniel Gonçalves <1151452@isep.ipp.pt>
2017-10-17 14:27:36 +0100

    refactores exercise 1 & 6 [PROJ1]

--- PROJ1/PROJ1.pl
+++ PROJ1/PROJ1.pl
@@ -1,221 +1,11 @@
-continente(africa).
-continente(americasul).
-continente(asia).
-continente(europa).
-continente(oceania).
+% [ PROJ1-01 ] Carregue para memória a BC definida no ficheiro
+% paises.txt.
+:- consult('paises.txt').
 
-%pais(pais,continente,Milhoes Habitantes)
-pais(argentina, americasul, 41.67).
-pais(bolivia, americasul, 9.70).
-pais(brasil, americasul, 206.12).
-pais(chile, americasul, 16.80).
-pais(colombia, americasul, 46.86).
-pais(equador, americasul, 14.88).
-pais(guiana, americasul, 0.07).
-pais(guianafrancesa, americasul, 2.88).
-pais(paraguai, americasul, 6.24).
-pais(peru, americasul, 28.22).
-pais(suriname, americasul, 0.04).
-pais(venezuela, americasul, 31.02).
-pais(uruguai, americasul, 3.35).
-pais(albania, europa, 2.88).
-pais(alemanha, europa, 82.8).
-pais(armenia, europa, 3.01).
-pais(austria, europa, 8.77).
-pais(belgica, europa, 11.37).
-pais(bielorussia, europa, 9.48).
-pais(bosnia, europa, 3.75).
-pais(bulgaria, europa, 7.1).
-pais(chipre, europa, 0.85).
-pais(croacia, europa, 4.15).
-pais(dinamarca, europa, 5.75).
-pais(eslovaquia, europa, 5.44).
-pais(eslovenia, europa, 2.06).
-pais(espanha, europa, 46.53).
-pais(estonia, europa, 1.32).
-pais(finlandia, europa, 5.5).
-pais(franca, europa, 66.99).
-pais(georgia, europa, 3.71).
-pais(grecia, europa, 10.76).
-pais(holanda, europa, 17.08).
-pais(hungria, europa, 9.8).
-pais(irlanda, europa, 4.77).
-pais(islandia, europa, 0.34).
-pais(italia, europa, 60.59).
-pais(kosovo, europa, 1.77).
-pais(letonia, europa, 1.98).
-pais(liechtenstein, europa, 0.04).
-pais(lituania, europa, 2.85).
-pais(luxemburgo, europa, 0.59).
-pais(macedonia, europa, 2.07).
-pais(malta, europa, 0.44).
-pais(moldavia, europa, 3.55).
-pais(monaco, europa, 0.04).
-pais(montenegro, europa, 0.62).
-pais(noruega, europa, 5.26).
-pais(polonia, europa, 38.42).
-pais(portugal, europa, 10.31).
-pais(reinounido, europa, 65.81).
-pais(republicacheca, europa, 10.57).
-pais(romenia, europa, 19.64).
-pais(russia, europa, 146.5).
-pais(servia, europa, 7.04).
-pais(suecia, europa, 10).
-pais(suica, europa, 8.42).
-pais(turquia, europa, 79.81).
-pais(ucrania, europa, 42.59).
-
-fronteira(argentina, bolivia).
-fronteira(argentina, brasil).
-fronteira(argentina, chile).
-fronteira(argentina, paraguai).
-fronteira(argentina, uruguai).
-
-fronteira(bolivia, brasil).
-fronteira(bolivia, chile).
-fronteira(bolivia, paraguai).
-fronteira(bolivia, peru).
-
-fronteira(brasil, colombia).
-fronteira(brasil, guiana).
-fronteira(brasil, guianafrancesa).
-fronteira(brasil, paraguai).
-fronteira(brasil, peru).
-fronteira(brasil, suriname).
-fronteira(brasil, uruguai).
-fronteira(brasil, venezuela).
-
-fronteira(chile, peru).
-
-fronteira(colombia, equador).
-fronteira(colombia, peru).
-fronteira(colombia, venezuela).
-
-fronteira(equador, peru).
-
-fronteira(guiana, suriname).
-fronteira(guiana, venezuela).
-
-fronteira(guianafrancesa, suriname).
-
-fronteira(albania, grecia).
-fronteira(albania, macedonia).
-fronteira(albania, montenegro).
-fronteira(albania, kosovo).
-
-fronteira(alemanha, austria).
-fronteira(alemanha, belgica).
-fronteira(alemanha, dinamarca).
-fronteira(alemanha, franca).
-fronteira(alemanha, holanda).
-fronteira(alemanha, luxemburgo).
-fronteira(alemanha, polonia).
-fronteira(alemanha, republicacheca).
-fronteira(alemanha, suica).
-
-fronteira(armenia, georgia).
-fronteira(armenia, turquia).
-
-fronteira(austria, eslovaquia).
-fronteira(austria, eslovenia).
-fronteira(austria, hungria).
-fronteira(austria, italia).
-fronteira(austria, liechtenstein).
-fronteira(austria, republicacheca).
-fronteira(austria, suica).
-
-fronteira(belgica, franca).
-fronteira(belgica, holanda).
-fronteira(belgica, luxemburgo).
-
-fronteira(bielorussia, letonia).
-fronteira(bielorussia, lituania).
-fronteira(bielorussia, polonia).
-fronteira(bielorussia, russia).
-fronteira(bielorussia, ucrania).
-
-fronteira(bosnia, croacia).
-fronteira(bosnia, montenegro).
-fronteira(bosnia, servia).
-
-fronteira(bulgaria, grecia).
-fronteira(bulgaria, macedonia).
-fronteira(bulgaria, romenia).
-fronteira(bulgaria, servia).
-fronteira(bulgaria, turquia).
-
-fronteira(croacia, eslovenia).
-fronteira(croacia, hungria).
-fronteira(croacia, montenegro).
-fronteira(croacia, servia).
-
-fronteira(eslovaquia, hungria).
-fronteira(eslovaquia, polonia).
-fronteira(eslovaquia, republicacheca).
-fronteira(eslovaquia, ucrania).
-
-fronteira(eslovenia, hungria).
-fronteira(eslovenia, italia).
-
-fronteira(espanha, franca).
-fronteira(espanha, portugal).
-
-fronteira(estonia, letonia).
-fronteira(estonia, russia).
-
-fronteira(finlandia, noruega).
-fronteira(finlandia, russia).
-fronteira(finlandia, suecia).
-
-fronteira(franca, italia).
-fronteira(franca, luxemburgo).
-fronteira(franca, monaco).
-fronteira(franca, suica).
-
-fronteira(georgia, russia).
-fronteira(georgia, turquia).
-
-fronteira(grecia, macedonia).
-fronteira(grecia, turquia).
-
-fronteira(hungria, romenia).
-fronteira(hungria, servia).
-fronteira(hungria, ucrania).
-
-fronteira(irlanda, reinounido).
-
-fronteira(italia, suica).
-
-fronteira(kosovo, macedonia).
-fronteira(kosovo, montenegro).
-fronteira(kosovo, servia).
-
-fronteira(letonia, lituania).
-fronteira(letonia, russia).
-
-fronteira(liechtenstein, suica).
-
-fronteira(lituania, polonia).
-fronteira(lituania, russia).
-
-fronteira(macedonia, servia).
-
-fronteira(moldavia, romenia).
-fronteira(moldavia, ucrania).
-
-fronteira(noruega, suecia).
-fronteira(noruega, russia).
-
-fronteira(montenegro, servia).
-
-fronteira(polonia, republicacheca).
-fronteira(polonia, russia).
-fronteira(polonia, ucrania).
-
-fronteira(romenia, servia).
-fronteira(romenia, ucrania).
-
-fronteira(russia, ucrania).
+% [PROJ1 - 02] Escreva o predicado lista(C) que lista todos os paíse% s
+% de um dado continente apresentando em
+% cabeçalho o continente e linha a linha os países: <nome>, <população>,
+% <lista países fronteira>.
 
 % Cria lista com paises de um dado continente
 paisesCont(C,L):- findall(P, pais(P,C, _), L).
@@ -246,13 +36,13 @@ maisPop([P|T],M0,M):- maxPop(M0,P,M1), maisPop(T,M1,M).
 maisPop(L,M):- [P|T] = L, maisPop(T,P,M).
 
 % Apaga a primeira instancia de um elemento numa lista
-delete(X, [X|T], T).
-delete(X, [H|T],[H|L]):- delete(X,T,L).
+del(X, [X|T], T).
+del(X, [H|T],[H|L]):- del(X,T,L).
 
 % Organiza todos os paises numa lista, encontra o pais com maior
 % população, retira esse pais da lista e repete o processo mais uma vez.
 % Isto permite obter os dois paises com maior população.
-doisMaisPop(P1,P2):-  findall(P, pais(P,_,_),L), write(L), maisPop(L,P1), delete(P1,L,LR), maisPop(LR,P2),!.
+doisMaisPop(P1,P2):-  findall(P, pais(P,_,_),L), write(L), maisPop(L,P1), del(P1,L,LR), maisPop(LR,P2),!.
 
 
 % [PROJ1-04] - Escreva o predicado paisesGrandes(C,N,L) que calcula a
@@ -278,14 +68,17 @@ somaPopViz(P,L,S):- findall((PL, P2),(vizinho(P, P2), pais(P2,_,PL)), L),somaPop
 
 % [PROJ1 - 6] Escreva o predicado numPaisesAtravessados(P1, P2, Num) que calcula o menor número de países que é necessário atravessar para chegar de P1 a P2.
 
+cont([],0).
+cont([_|T],N):- cont(T,N1), N is N1+1.
+
 % Pesquisa em Largura:
-bfsComContador(Orig, Dest, Num):-
-    bfsComContador2(Dest,[[Orig]], Num).
+npa(Orig, Dest,Num):-
+    npa2(Dest,[[Orig]],Num).
 % Condição final: destino = nó à cabeça do caminho actual
-bfsComContador2(Dest, [[Dest|_]|_],-1):-
+npa2(Dest, [[Dest|T]|_],Num):-
     % Retirar a visita ao pais de destino (por isso, -1).
-    !.
-bfsComContador2(Dest, [LA|Outros], Num):-
+    cont([Dest|T],Num1), Num is Num1 - 2.
+npa2(Dest, [LA|Outros],Num):-
     LA = [Act|_],
     % Calcular todos os nós adjacentes não visitado e
     % gerar um caminhos novo c/ cada nó e caminho actual
@@ -296,10 +89,44 @@ bfsComContador2(Dest, [LA|Outros], Num):-
     % P/ posterior exploração
     append(Outros, Novos, Todos),
     % Chamada recursiva
-    bfsComContador2(Dest, Todos, Num1),
-    Num is Num1 + 1.
+    npa2(Dest, Todos,Num).
 
 numPaisesAtravessados(P1, P2, Num):-
     pais(P1, C, _),
     pais(P2, C, _), % Verifica se pertence ao mesmo continente.
-    bfsComContador(P1, P2, Num).
+    npa(P1, P2, Num).
+
+% [PROJ1 - 08] Dado um continente, um país origem e o número de fronteiras a atravessar, apresente todos os roteiros possíveis.
+%
+
+% Reverse de lista de listas
+%
+imprimeReverse([]).
+imprimeReverse([H|T]):- imprimeReverse(T), reverse(H, Rot), write(Rot), write('\n').
+
+% roteiros (com base no bfs)
+roteirosSemDest(Orig, Continente, Num) :-
+    pais(Orig,Continente,_),
+
+
+
+
+    rsd2([[Orig]], Continente, Num) .
+%condição final: destino = nó à cabeça do caminho actual
+rsd2(Todos,_, 0) :-
+    %caminho actual está invertido
+    imprimeReverse(Todos).
+rsd2([LA|Outros],Continente, Num) :-
+    LA = [Act|_] ,
+    %calcular todos os nós adjacentes não visitado e
+    %gerar um caminhos novo c/ cada nó e caminho actual
+    findall([X|LA],
+            (pais(X,Continente,_), vizinho(Act, X), \+ member(X,LA)),
+            Novos),
+    %novos caminhos são colocados no final da lista
+    %p/ posterior exploração
+    append(Outros, Novos, Todos) ,
+    Num1 is Num - 1,
+    %chamada recursiva
+    rsd2(Todos,Continente, Num1) .
+
---
+++ PROJ1/paises.txt
@@ -0,0 +1,218 @@
+continente(africa).
+continente(americasul).
+continente(asia).
+continente(europa).
+continente(oceania).
+
+%pais(pais,continente,Milhoes Habitantes)
+pais(argentina, americasul, 41.67).
+pais(bolivia, americasul, 9.70).
+pais(brasil, americasul, 206.12).
+pais(chile, americasul, 16.80).
+pais(colombia, americasul, 46.86).
+pais(equador, americasul, 14.88).
+pais(guiana, americasul, 0.07).
+pais(guianafrancesa, americasul, 2.88).
+pais(paraguai, americasul, 6.24).
+pais(peru, americasul, 28.22).
+pais(suriname, americasul, 0.04).
+pais(venezuela, americasul, 31.02).
+pais(uruguai, americasul, 3.35).
+pais(albania, europa, 2.88).
+pais(alemanha, europa, 82.8).
+pais(armenia, europa, 3.01).
+pais(austria, europa, 8.77).
+pais(belgica, europa, 11.37).
+pais(bielorussia, europa, 9.48).
+pais(bosnia, europa, 3.75).
+pais(bulgaria, europa, 7.1).
+pais(chipre, europa, 0.85).
+pais(croacia, europa, 4.15).
+pais(dinamarca, europa, 5.75).
+pais(eslovaquia, europa, 5.44).
+pais(eslovenia, europa, 2.06).
+pais(espanha, europa, 46.53).
+pais(estonia, europa, 1.32).
+pais(finlandia, europa, 5.5).
+pais(franca, europa, 66.99).
+pais(georgia, europa, 3.71).
+pais(grecia, europa, 10.76).
+pais(holanda, europa, 17.08).
+pais(hungria, europa, 9.8).
+pais(irlanda, europa, 4.77).
+pais(islandia, europa, 0.34).
+pais(italia, europa, 60.59).
+pais(kosovo, europa, 1.77).
+pais(letonia, europa, 1.98).
+pais(liechtenstein, europa, 0.04).
+pais(lituania, europa, 2.85).
+pais(luxemburgo, europa, 0.59).
+pais(macedonia, europa, 2.07).
+pais(malta, europa, 0.44).
+pais(moldavia, europa, 3.55).
+pais(monaco, europa, 0.04).
+pais(montenegro, europa, 0.62).
+pais(noruega, europa, 5.26).
+pais(polonia, europa, 38.42).
+pais(portugal, europa, 10.31).
+pais(reinounido, europa, 65.81).
+pais(republicacheca, europa, 10.57).
+pais(romenia, europa, 19.64).
+pais(russia, europa, 146.5).
+pais(servia, europa, 7.04).
+pais(suecia, europa, 10).
+pais(suica, europa, 8.42).
+pais(turquia, europa, 79.81).
+pais(ucrania, europa, 42.59).
+
+fronteira(argentina, bolivia).
+fronteira(argentina, brasil).
+fronteira(argentina, chile).
+fronteira(argentina, paraguai).
+fronteira(argentina, uruguai).
+
+fronteira(bolivia, brasil).
+fronteira(bolivia, chile).
+fronteira(bolivia, paraguai).
+fronteira(bolivia, peru).
+
+fronteira(brasil, colombia).
+fronteira(brasil, guiana).
+fronteira(brasil, guianafrancesa).
+fronteira(brasil, paraguai).
+fronteira(brasil, peru).
+fronteira(brasil, suriname).
+fronteira(brasil, uruguai).
+fronteira(brasil, venezuela).
+
+fronteira(chile, peru).
+
+fronteira(colombia, equador).
+fronteira(colombia, peru).
+fronteira(colombia, venezuela).
+
+fronteira(equador, peru).
+
+fronteira(guiana, suriname).
+fronteira(guiana, venezuela).
+
+fronteira(guianafrancesa, suriname).
+
+fronteira(albania, grecia).
+fronteira(albania, macedonia).
+fronteira(albania, montenegro).
+fronteira(albania, kosovo).
+
+fronteira(alemanha, austria).
+fronteira(alemanha, belgica).
+fronteira(alemanha, dinamarca).
+fronteira(alemanha, franca).
+fronteira(alemanha, holanda).
+fronteira(alemanha, luxemburgo).
+fronteira(alemanha, polonia).
+fronteira(alemanha, republicacheca).
+fronteira(alemanha, suica).
+
+fronteira(armenia, georgia).
+fronteira(armenia, turquia).
+
+fronteira(austria, eslovaquia).
+fronteira(austria, eslovenia).
+fronteira(austria, hungria).
+fronteira(austria, italia).
+fronteira(austria, liechtenstein).
+fronteira(austria, republicacheca).
+fronteira(austria, suica).
+
+fronteira(belgica, franca).
+fronteira(belgica, holanda).
+fronteira(belgica, luxemburgo).
+
+fronteira(bielorussia, letonia).
+fronteira(bielorussia, lituania).
+fronteira(bielorussia, polonia).
+fronteira(bielorussia, russia).
+fronteira(bielorussia, ucrania).
+
+fronteira(bosnia, croacia).
+fronteira(bosnia, montenegro).
+fronteira(bosnia, servia).
+
+fronteira(bulgaria, grecia).
+fronteira(bulgaria, macedonia).
+fronteira(bulgaria, romenia).
+fronteira(bulgaria, servia).
+fronteira(bulgaria, turquia).
+
+fronteira(croacia, eslovenia).
+fronteira(croacia, hungria).
+fronteira(croacia, montenegro).
+fronteira(croacia, servia).
+
+fronteira(eslovaquia, hungria).
+fronteira(eslovaquia, polonia).
+fronteira(eslovaquia, republicacheca).
+fronteira(eslovaquia, ucrania).
+
+fronteira(eslovenia, hungria).
+fronteira(eslovenia, italia).
+
+fronteira(espanha, franca).
+fronteira(espanha, portugal).
+
+fronteira(estonia, letonia).
+fronteira(estonia, russia).
+
+fronteira(finlandia, noruega).
+fronteira(finlandia, russia).
+fronteira(finlandia, suecia).
+
+fronteira(franca, italia).
+fronteira(franca, luxemburgo).
+fronteira(franca, monaco).
+fronteira(franca, suica).
+
+fronteira(georgia, russia).
+fronteira(georgia, turquia).
+
+fronteira(grecia, macedonia).
+fronteira(grecia, turquia).
+
+fronteira(hungria, romenia).
+fronteira(hungria, servia).
+fronteira(hungria, ucrania).
+
+fronteira(irlanda, reinounido).
+
+fronteira(italia, suica).
+
+fronteira(kosovo, macedonia).
+fronteira(kosovo, montenegro).
+fronteira(kosovo, servia).
+
+fronteira(letonia, lituania).
+fronteira(letonia, russia).
+
+fronteira(liechtenstein, suica).
+
+fronteira(lituania, polonia).
+fronteira(lituania, russia).
+
+fronteira(macedonia, servia).
+
+fronteira(moldavia, romenia).
+fronteira(moldavia, ucrania).
+
+fronteira(noruega, suecia).
+fronteira(noruega, russia).
+
+fronteira(montenegro, servia).
+
+fronteira(polonia, republicacheca).
+fronteira(polonia, russia).
+fronteira(polonia, ucrania).
+
+fronteira(romenia, servia).
+fronteira(romenia, ucrania).
+
+fronteira(russia, ucrania).
